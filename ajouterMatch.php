e3a891669a9cbb2ad326b4c1404941652740c8b0
¨Maxime F <maxime.fabre10@gmail.com>
2018-12-17 18:58:33 +0100

    Creatin match + ajout

---
+++ ajouterMatch.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+    <head>
+	     <!-- Required meta tags -->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <title>Ajouter un match</title>
+    </head>
+	<body>
+
+		<?php
+    		require('header.php');
+			require('lib.php');
+			$tab = array();
+
+			if((empty($_POST['DateR'])||empty($_POST['Lieu'])||empty($_POST['Adversaire'])||empty($_POST['RNous'])||empty($_POST['REux']))&&isset($_POST['Ajouter'])){
+				echo("Veuillez renseigner tout les champs du formulaire correctement");
+				formulaireMatch();
+			}
+			else{
+				if(isset($_POST['Ajouter'])) {
+					
+					//Connexion à la BDD
+					$linkpdo=connecterPDO();
+				
+					$DateR=sécurisationVariable($_POST['DateR']);
+					$Lieu=sécurisationVariable($_POST['Lieu']);
+					$Adversaire=sécurisationVariable($_POST['Adversaire']);
+					$RNous=sécurisationVariable($_POST['RNous']);
+					$REux=sécurisationVariable($_POST['REux']);
+
+					$linkpdo=connecterPDO();
+					
+					//Préparation requête ajout
+					$reqAjout = $linkpdo->prepare("INSERT INTO rencontre (
+												DateRencontre,
+												LieuRencontre,
+												EquipeAdverse,
+												ResultatEquipe,
+												ResultatAdverse) 
+												VALUES (:DateR,
+												:Lieu,
+												:Adversaire,
+												:RNous,
+												:REux");
+					//Exécution requête ajout
+					$tab_param = array(
+					'DateR'=>$DateR,
+					'Lieu'=>$Lieu,
+					'Adversaire'=>$Adversaire,
+					'RNous'=>$RNous,
+					'REux'=>$REux);
+					$reqAjout->execute($tab_param);
+					echo("Le match contre $Adversaire a bien été ajouté<br/>");
+					?>
+					<br/>
+                    <a class="btn btn-light" href=javascript:history.go(-1) role="button">Retour</a>
+                    <?php
+				}
+				else {
+					formulaireMatch();
+				}
+			}
+		?>
+	</body>
+</html>
--- match.php
+++ match.php
@@ -1,21 +1,56 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!DOCTYPE HTML>
+<html lang="fr">
+    <head>
+	     <!-- Required meta tags -->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-	<title>Match</title>
-</head>
-<body>
-	<?php 
-		require('lib.php');
+        <title>Matchs</title>
+    </head>
+	<body>
+	<?php
 		require('header.php');
+	?>
+    <h2 style="text-align:center">Matchs joués</h2>
+	<table class="table">
+		<thead class="thead-dark">
+			<tr>
+				<!--<td>Photo</td>-->
+				<th scope="col">Date</th>
+				<th scope="col">Lieu</th>
+				<th scope="col">Adversaire</th>
+				<th scope="col">Score (Nous - Eux)</th>
+                <th scope="col"> </th><!-- Truc pour supprimer-->
 
-	 ?>
-	SALUT C'EST COOL !
+			</tr>
+		</thead>
+		<?php
+			require('lib.php');
+			$linkpdo=connecterPDO();?>
+<form method=POST action="">
+    <?php
+			//Préparation de la requête
+			$res = $linkpdo->prepare('SELECT * FROM rencontre');
+			$res->execute(array());
+			while(($data = $res->fetch())) {?>
+			<tr>
+				<td><?php echo"$data[1]"?></td>
+				<td><?php echo"$data[2]"?></td>
+				<td><?php echo"$data[3]"?></td>
+				<td><?php echo"$data[4] - $data[5]"?></td>
+                <td><a href=<?php echo "supprimerMatch.php?IdMatch=$data[0]";?>>Supprimer</a></td>
+			</tr>
+        </form>
+			<?php }
+			
+			//On ferme le curseur
+			$res->closeCursor();
+		?>
 
-</body>
+		</table>
+    <a style="background-color: #818181;" class="btn btn-secondary btn-lg btn-block" href="ajouterMatch.php" role="button">Ajouter match</a>	<!-- Création du tableau-->
+    </body>
 </html>

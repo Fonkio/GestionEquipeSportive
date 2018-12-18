c63614f46b018e0d49d31cef403da64d02124a20
¨Maxime F <maxime.fabre10@gmail.com>
2018-12-18 08:56:40 +0100

    Begin le modal

--- match.php
+++ match.php
@@ -1,21 +1,20 @@
 <!DOCTYPE HTML>
 <html lang="fr">
     <head>
-	     <!-- Required meta tags -->
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	    <!-- Bootstrap CSS -->
-	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-        <title>Matchs</title>
-    </head>
+		<title>Matchs</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	</head>
 	<body>
 	<?php
 		require('header.php');
 	?>
 </br>
-    <h2 style="text-align:center">Matchs joués</h2></br>
+    <h2 style="text-align:center">Liste des matchs</h2></br>
 	<table class="table">
 		<thead class="thead-dark">
 			<tr>

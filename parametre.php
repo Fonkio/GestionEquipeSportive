07a24b017bd2454524ccf174c65de4db7ffa51e1
Aleridia <maxdu34.m@gmail.com>
2018-12-18 09:29:30 +0100

    Changement de parametre.php

--- parametre.php
+++ parametre.php
@@ -15,51 +15,46 @@
         require('header.php');
         require('lib.php');
         $linkpdo=connecterPDO();
-
-        //Génération de la requête de recherche
-        $reqRecherche = $linkpdo -> prepare("SELECT * FROM identifiant WHERE id=:id");
-        $reqRecherche -> execute(array('id'=>1));
-        while($data=$reqRecherche->fetch()){
-            $Login=$data['Login'];
-            $Mdp=$data['Mdp'];
-        }
-	
 	//On gère le login
         if(isset($_POST['Modifier_Login'])){
             //Sécurisation du login entré
+            $Login=rechercheLogin();
             $tmp=sécurisationVariable($_POST['Login']);
 
             if($tmp==$Login){
-                echo "C'est le même login";
+                $string = "C'est le même login";
             }
             else{
-                $Login=$tmp;
                 $reqModif = $linkpdo -> prepare("UPDATE identifiant SET Login=:tmp WHERE id=:id");
                 $reqModif -> execute(array('id'=>1,'tmp'=>$tmp));
-                echo "Login changé";
+                $string = "Login changé";
             }
+        }
+        else {
+        $string="";
         }
 
 	//On gère le mdp
         if(isset($_POST['Modifier_Passwd'])){
             //Sécurisation du mdp entré
             $tmp=sécurisationVariable($_POST['Mdp']);
+            $Mdp=rechercheMdp();
             
             if($tmp==$Mdp){
-                echo "C'est le même mot de passe";
+                $string = "C'est le même mot de passe";
             }
             else{
-                $Mdp=$tmp;
                 $reqModif = $linkpdo -> prepare("UPDATE identifiant SET Mdp=:tmp WHERE id=:id");
                 $reqModif -> execute(array('id'=>1,'tmp'=>$tmp));
-                echo "Mdp changé";
+                $string = "Mdp changé";
             }
         }
+
     ?>
     
     <h3>Changer le login :</h3>
     <form action="" method="POST">
-        <input type="text" name="Login" value="<?php echo"$Login";?>" />
+        <input type="text" name="Login" />
         <button class="btn btn-primary" type="submit" name="Modifier_Login">Modifier</button>
     </form>
 
@@ -69,5 +64,10 @@
         <input type="password" name="Mdp" />
         <button class="btn btn-primary" type="submit" name="Modifier_Passwd">Modifier</button>
     </form>
+    
+    <?php
+    	echo $string;
+    ?>
+    
 </body>
 </html>

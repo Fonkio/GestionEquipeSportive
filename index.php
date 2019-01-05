0bc9262a1fca96c3b24127119e35b3c97ef111b0
Aleridia <maxdu34.m@gmail.com>
2019-01-05 14:26:11 +0100

    Pourcentages fait dans les stats

--- index.php
+++ index.php
@@ -6,17 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Accueil</title>
 </head>
-<body style="color:black; background-image:url(https://amicalecoteauessert.files.wordpress.com/2017/05/petanque.jpg); background-repeat:no-repeat;">
-    <?php
-       require('header.php');
-       require('lib.php');
-    ?> <div style="border-radius: 20px; margin-top: 100px; margin-left: 40px; margin-right: 40px;margin-bottom: 40px; background-color: rgba(255, 255, 255, .8); padding: 40px;">
-    <h1 style="text-align:center;">Mettre les stats ici</h1>
-	
+<body style="color:black; background-image:url(https://amicalecoteauessert.files.wordpress.com/2017/05/petanque.jpg); background-repeat:repeat;">
+<div style="border-radius: 20px; margin-top: 100px; margin-left: 40px; margin-right: 40px;margin-bottom: 40px; background-color: rgba(255, 255, 255, .8); padding: 40px;">
+    <h1 style="text-align:center;">Statistiques</h1>
+<?php
+    require('header.php');
+    require('lib.php');
+    $linkpdo = connecterPDO();
+
+    //Requête pour le nombre total de matchs joués
+    $reqRechercheTotal = $linkpdo -> prepare("SELECT count(*) FROM rencontre WHERE ResultatEquipe is not null AND ResultatAdverse is not null");
+    $reqRechercheTotal -> execute();
+    while ($data = $reqRechercheTotal -> fetch()) {
+        $nbMatchTotal=$data['count(*)'];
+    }
+
+    //Requête pour le nombre de victoire
+    $reqRechercheWin = $linkpdo -> prepare("SELECT count(*) FROM rencontre WHERE ResultatEquipe is not null AND ResultatAdverse is not null AND ResultatEquipe > ResultatAdverse");
+    $reqRechercheWin -> execute();
+    while ($data = $reqRechercheWin -> fetch()) {
+        $nbMatchWin=$data['count(*)'];
+    }
+
+    //Requête pour le nombre de nuls
+    $reqRechercheNul = $linkpdo -> prepare("SELECT count(*) FROM rencontre WHERE ResultatEquipe is not null AND ResultatAdverse is not null AND ResultatEquipe = ResultatAdverse");
+    $reqRechercheNul -> execute();
+    while ($data = $reqRechercheNul -> fetch()) {
+        $nbMatchNul=$data['count(*)'];
+    }
+
+    //Requête pour le nombre de lose
+    $reqRechercheLose = $linkpdo -> prepare("SELECT count(*) FROM rencontre WHERE ResultatEquipe is not null AND ResultatAdverse is not null AND ResultatEquipe < ResultatAdverse");
+    $reqRechercheLose -> execute();
+    while ($data = $reqRechercheLose -> fetch()) {
+        $nbMatchLose=$data['count(*)'];
+    }
+
+    //Calcul des pourcentages :
+    $prctWin = ($nbMatchWin/$nbMatchTotal)*100;
+    $prctNul = ($nbMatchNul/$nbMatchTotal)*100;
+    $prctLose = ($nbMatchLose/$nbMatchTotal)*100;
+?>
+    <h2>Nombre match : <?php echo $nbMatchTotal;?> </h2>
+    <h2>Nombre de victoires : <?php echo $nbMatchWin." (".$prctWin."%)"; ?> </h2>
+    <h2>Nombre de nuls : <?php echo $nbMatchNul." (".$prctNul."%)"; ?> </h2>
+    <h2>Nombre de perdus : <?php echo $nbMatchLose." (".$prctLose."%)"; ?> </h2>
+</div>
 </body>
 </html>
 
--- modifierJoueur.php
+++ modifierJoueur.php
@@ -6,53 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Modifier un joueur</title>
 </head>
 <body style="color:black; background-image:url(https://amicalecoteauessert.files.wordpress.com/2017/05/petanque.jpg); background-repeat:no-repeat;">
-    <?php
-        require('lib.php');
-        require('header.php');
-        $linkpdo=connecterPDO(); ?><div style="border-radius: 20px; margin-top: 100px; margin-left: 40px; margin-right: 40px;margin-bottom: 40px; background-color: rgba(255, 255, 255, .8); padding: 40px;"> <?php
-	//Variables pour remplir le formulaire :
-        $id=sécurisationVariable($_GET['NumLicence']);
+<?php
+    require('lib.php');
+    require('header.php');
+    $linkpdo = connecterPDO();
+?>
+<div style="border-radius: 20px; margin-top: 100px; margin-left: 40px; margin-right: 40px;margin-bottom: 40px; background-color: rgba(255, 255, 255, .8); padding: 40px;"> <?php
+    //Variables pour remplir le formulaire :
+    $id = sécurisationVariable($_GET['NumLicence']);
 
-    	//Requête de modification
-        if(isset($_POST['Ajouter'])) {
-        $reqModif = $linkpdo -> prepare("UPDATE joueur SET Nom=:Nom, Prenom =:Prenom, DateDeNaissance=:Ddn, Taille=:Taille, Poids=:Poids, PostePref=:PostePref, Statut=:Statut WHERE NumLicence=:NumLicence");
-        $reqModif -> execute(array('Nom'=>sécurisationVariable($_POST['Nom']),
-				                 'Prenom'=>sécurisationVariable($_POST['Prenom']),
-                                 'Ddn'=>sécurisationVariable($_POST['Ddn']),
-				                 'Taille'=>sécurisationVariable($_POST['Taille']),
-				                 'Poids'=>sécurisationVariable($_POST['Poids']),
-				                 'PostePref'=>sécurisationVariable($_POST['PostePref']),
-				                 'Statut'=>sécurisationVariable($_POST['Statut']),
-				                 'NumLicence'=>sécurisationVariable($_POST['NumLicence'])));
+    //Requête de modification
+    if (isset($_POST['Ajouter'])) {
+        $reqModif = $linkpdo->prepare("UPDATE joueur SET Nom=:Nom, Prenom =:Prenom, DateDeNaissance=:Ddn, Taille=:Taille, Poids=:Poids, PostePref=:PostePref, Statut=:Statut WHERE NumLicence=:NumLicence");
+        $reqModif->execute(array('Nom' => sécurisationVariable($_POST['Nom']),
+            'Prenom' => sécurisationVariable($_POST['Prenom']),
+            'Ddn' => sécurisationVariable($_POST['Ddn']),
+            'Taille' => sécurisationVariable($_POST['Taille']),
+            'Poids' => sécurisationVariable($_POST['Poids']),
+            'PostePref' => sécurisationVariable($_POST['PostePref']),
+            'Statut' => sécurisationVariable($_POST['Statut']),
+            'NumLicence' => sécurisationVariable($_POST['NumLicence'])));
         echo "Joueur modifié";
-	}
+    }
 
-	//Requête de recherche
-        $reqRecherche = $linkpdo -> prepare("SELECT * FROM joueur WHERE NumLicence = :id");
-        $reqRecherche -> execute(array('id'=>$id));
+    //Requête de recherche
+    $reqRecherche = $linkpdo->prepare("SELECT * FROM joueur WHERE NumLicence = :id");
+    $reqRecherche->execute(array('id' => $id));
 
-	//Initialisation dans un tableau
-      	while($data=$reqRecherche->fetch()){
-		$tab = array('NumLicence' => $data['NumLicence'],
-			     'Nom' => $data['Nom'],
-			     'Prenom' => $data['Prenom'],
-			     'Ddn' => $data['DateDeNaissance'],
-			     'Taille' => $data['Taille'],
-			     'Poids' => $data['Poids'],
-			     'PostePref' => $data['PostePref'],
-			     'Statut' => $data['Statut']);
-	}
-	formulaire("modifierJoueur.php?NumLicence=$id", $tab, "Modifier");
+    //Initialisation dans un tableau
+    while ($data = $reqRecherche->fetch()) {
+        $tab = array('NumLicence' => $data['NumLicence'],
+            'Nom' => $data['Nom'],
+            'Prenom' => $data['Prenom'],
+            'Ddn' => $data['DateDeNaissance'],
+            'Taille' => $data['Taille'],
+            'Poids' => $data['Poids'],
+            'PostePref' => $data['PostePref'],
+            'Statut' => $data['Statut']);
+    }
+    formulaire("modifierJoueur.php?NumLicence=$id", $tab, "Modifier");
 
-        if(isset($_FILES['Image'])){
-            $maxsize=$_POST['MAX_FILE_SIZE'];
-            uploadImage($id,$maxsize);
-        }
+    if (isset($_FILES['Image'])) {
+        $maxsize = $_POST['MAX_FILE_SIZE'];
+        uploadImage($id, $maxsize);
+    }
     ?>
 </div>
 </body>
